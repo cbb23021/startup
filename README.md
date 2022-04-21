d8eec867c4d95545f76f1f1d811395d028a9a841
Michael Chou <snoopy02m@yahoo.com.tw>
2022-04-21 14:53:09 +0400

    Add zsh-vi-mode

--- README.md
+++ README.md
@@ -3,6 +3,13 @@
 ### quick setup mac computer
 
 ### log
+
+
 #### 2022-04-13
+
+- Add zsh-vi-mode
+
+#### 2022-04-13
+
 - init
 - Add startup files
--- startup.sh
+++ startup.sh
@@ -74,6 +74,8 @@ git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zs
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 # 安裝 zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# 安裝 zsh-vi-mode
+git clone https://github.com/jeffreytse/zsh-vi-mode ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-vi-mode
 
 # ======================== #
 # === 啟動iTerm ========== #
